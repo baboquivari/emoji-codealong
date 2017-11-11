3a0366c4156600f74abd2f875ae2b6c7785aa8f8
unknown <to.sian.san@gmail.com>
2017-11-11 17:09:33 +0000

    changed variable name 'wait' to 'delay'

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-export function debounce (func, wait, context) {
+export function debounce (func, delay, context) {
     var previousCallTime;
     var firstCall = true;
     var id; // we store the id of each setTimeout in here, so that we can cancel it later if needs be
@@ -9,11 +9,11 @@ export function debounce (func, wait, context) {
         var startTimer = function () {
             id = setTimeout(function () {
                 func.call(context, newArray);
-            }, wait)
+            }, delay)
         }
 
         if (firstCall) {
-            // it's the first call, so we start the timer (based on the 'wait' parameter up top) which is set up to fire our 'func()' once the timer is up...
+            // it's the first call, so we start the timer (based on the 'delay' parameter up top) which is set up to fire our 'func()' once the timer is up...
             startTimer();      
             // we set 'firstCall' to false so this 'if' block never runs again      
             firstCall = false;
@@ -23,15 +23,15 @@ export function debounce (func, wait, context) {
             return;
         }
 
-        // if another call comes in before 1 second ('wait') is up... 
-        if (timeSinceLastCall < wait) {
+        // if another call comes in before 1 second ('delay') is up... 
+        if (timeSinceLastCall < delay) {
             // ... cancel the previous setTimeout that is waiting to fire 'func()'
             clearTimeout(id);
             // ... then restart the setTimeout 
             startTimer();
             previousCallTime = now;
         } else {
-            // but if a subsequent call comes in AFTER 'wait' amount of time, then our function will have been allowed to fire. Now, we can restart the timer again.
+            // but if a subsequent call comes in AFTER 'delay' amount of time, then our function will have been allowed to fire. Now, we can restart the timer again.
             previousCallTime = now;
             startTimer();
         }
